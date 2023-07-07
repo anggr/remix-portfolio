a92fd1bdc001ec7c9d5261960fb5512fa1909185
anggr <anggorookky@gmail.com>
2023-07-07 17:00:55 +0800

    test: Add unit test for Login component

--- tests/unit/login.test.tsx
+++ tests/unit/login.test.tsx
@@ -1,10 +1,17 @@
 import { test, expect, vi } from 'vitest'
-import { render, fireEvent, screen, waitFor } from '@testing-library/react'
-import Login from '../app/routes/login'
+import { render, fireEvent, screen } from '@testing-library/react'
+import Login from '../../app/routes/login'
+// Mock the session.server module
+vi.mock('~/session.server', () => ({
+  getSession: () => ({ user: null }), // Replace with the expected session data
+}))
 
 test('submits form and calls onSubmit with form data', async () => {
+  // Create a mock onSubmit function
   const onSubmit = vi.fn()
-  render(<Login />)
+
+  // Pass the mock function as a prop when you render the component
+  render(<Login onSubmit={onSubmit} />)
 
   const emailInput = screen.getByLabelText(/email/i)
   const passwordInput = screen.getByLabelText(/password/i)
@@ -14,8 +21,9 @@ test('submits form and calls onSubmit with form data', async () => {
   fireEvent.change(passwordInput, { target: { value: 'testpassword' } })
   fireEvent.click(submitButton)
 
-  await waitFor(() => expect(onSubmit).toHaveBeenCalledWith({
+  // Check that the onSubmit function was called with the correct data
+  expect(onSubmit).toBeCalledWith({
     email: 'testuser@example.com',
     password: 'testpassword',
-  }))
+  })
 })
