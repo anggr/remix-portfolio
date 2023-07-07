e1dae9b5b16b26a71c20c466c1240eedd4cb807f
anggr <anggorookky@gmail.com>
2023-07-07 18:08:56 +0800

    test: adds login form render and input update tests

--- tests/unit/login.test.tsx
+++ tests/unit/login.test.tsx
@@ -1,29 +1,49 @@
-import { test, expect, vi } from 'vitest'
-import { render, fireEvent, screen } from '@testing-library/react'
-import Login from '../../app/routes/login'
+import { test, expect, vi } from "vitest";
+import { render, fireEvent, screen } from "@testing-library/react";
+import Login from "../../app/routes/login";
+
 // Mock the session.server module
-vi.mock('~/session.server', () => ({
-  getSession: () => ({ user: null }), // Replace with the expected session data
-}))
+vi.mock("~/models/user.server", () => ({
+  getUserById: async () => null,
+  getUserByEmail: async () => null,
+  createUser: async () => null,
+  deleteUserByEmail: async () => null,
+  verifyLogin: async () => null,
+}));
 
-test('submits form and calls onSubmit with form data', async () => {
-  // Create a mock onSubmit function
-  const onSubmit = vi.fn()
+test("renders form fields and buttons", async () => {
+  render(<Login />);
 
-  // Pass the mock function as a prop when you render the component
-  render(<Login onSubmit={onSubmit} />)
+  // Check that the email input field is rendered
+  const emailInput = screen.getByLabelText(/email address/i);
+  expect(emailInput).not.toBeNull();
 
-  const emailInput = screen.getByLabelText(/email/i)
-  const passwordInput = screen.getByLabelText(/password/i)
-  const submitButton = screen.getByText(/login/i)
+  // Check that the password input field is rendered
+  const passwordInput = screen.getByLabelText(/password/i);
+  expect(passwordInput).not.toBeNull();
 
-  fireEvent.change(emailInput, { target: { value: 'testuser@example.com' } })
-  fireEvent.change(passwordInput, { target: { value: 'testpassword' } })
-  fireEvent.click(submitButton)
+  // Check that the login button is rendered
+  const loginButton = screen.getByText(/log in/i);
+  expect(loginButton).not.toBeNull();
 
-  // Check that the onSubmit function was called with the correct data
-  expect(onSubmit).toBeCalledWith({
-    email: 'testuser@example.com',
-    password: 'testpassword',
-  })
-})
+  // Check that the signup button is rendered
+  const signupButton = screen.getByText(/sign up/i);
+  expect(signupButton).not.toBeNull();
+});
+
+test("updates form field values when they are changed", async () => {
+  render(<Login />);
+
+  const emailInput = screen.getByLabelText(
+    /email address/i
+  ) as HTMLInputElement;
+  const passwordInput = screen.getByLabelText(/password/i) as HTMLInputElement;
+
+  // Change the value of the email input field
+  fireEvent.change(emailInput, { target: { value: "testuser@example.com" } });
+  expect(emailInput.value).toBe("testuser@example.com");
+
+  // Change the value of the password input field
+  fireEvent.change(passwordInput, { target: { value: "testpassword" } });
+  expect(passwordInput.value).toBe("testpassword");
+});
