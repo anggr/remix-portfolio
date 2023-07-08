7263aa306736780bff33f10e8406651304234754
anggr <anggorookky@gmail.com>
2023-07-08 09:05:04 +0800

    feat(store): add persistent state storage

--- store/store.ts
+++ store/store.ts
@@ -2,9 +2,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer'; // import the root reducer
 
+const preloadedState = typeof window !== 'undefined' && localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')!) : {};
+
 const store = configureStore({
   reducer: rootReducer,
+  preloadedState,
 });
+
+store.subscribe(() => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+  }
+})
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
