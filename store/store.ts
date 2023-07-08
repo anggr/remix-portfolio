0ebaee059e3d5e9a34069b505a2b2799d89e81b0
anggr <anggorookky@gmail.com>
2023-07-08 08:05:58 +0800

    chore(store): add serializableCheck option

--- store/store.ts
+++ store/store.ts
@@ -13,6 +13,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST'],
+      },
+    }),
 });
 
 export type AppDispatch = typeof store.dispatch;
